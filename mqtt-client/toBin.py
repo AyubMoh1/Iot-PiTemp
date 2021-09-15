22442dd68d711105e1ad8d5d4b024d6b307b7a5b
Ayub <ayub.mohyadin@gmail.com>
2021-09-15 20:48:51 +0200

    pyfil för göra log till binär

---
+++ mqtt-client/toBin.py
@@ -0,0 +1,7 @@
+with open("log.txt", "r") as file:
+    for i in range(0, 20):
+        file.write(str(i))
+
+with open("data.bin", "wb") as file:
+    for i in range(0, 20):
+        file.write(i.to_bytes(2, byteorder="little"))
