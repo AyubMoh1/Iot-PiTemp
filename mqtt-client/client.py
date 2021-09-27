a0e47354c0c14ef24bbe0ae4a6d61ffc5f9fc94c
AyubMoh1 <89800658+AyubMoh1@users.noreply.github.com>
2021-09-27 09:45:18 +0200

    Update client.py

--- mqtt-client/client.py
+++ mqtt-client/client.py
@@ -1,18 +1,32 @@
+"""Author: Alexander Mörch"""
+
+#Modul som läser och omvandlar värdet från en temperaturgivare
+import dht as temp
+#Modul som hanterar MQTT-implementation
 import paho.mqtt.client as mqtt
-import time 
+import time
 
-# Create a MQTT client and register a callback
-# for connect events
+#Skapar en MQTT-klient
 client = mqtt.Client()
-client.on_connect = on_connect
 
-# Connect to a broker
-client.connect("localhost", port=1883, keepalive=60)
+#Ansluter till en broker --> HiveMQ idetta fallet
+#client.connect("broker.hivemq.com", port=1883, keepalive=60)
+client.connect("192.168.1.204",port=1883, keepalive=60)
 
-# Start a background loop that handles all
-# communication with the MQTT broker
+#Startar bakgrundsprocess som hanterar kommunikation med brokern
 client.loop_start()
 
-# send a random value every second
 while True:
-    client.publish("some/topic", payload=data, qos=1)
+    #Läser data från temperaturgivaren --> Se tempguage-modulen
+    var = temp.result.temperature
+
+    #Anger topic, vad som skall skickas, och quality of service
+    msg1 = client.publish("yrgo/ela20/Dark/hello", payload=var, qos=1)
+
+    #Väntar en sekund
+    time.sleep(1)
+
+#Väntar tills meddelandet är publicerat
+msg1.wait_for_publish()
+#Bryter koppling till broker
+client.disconnect()
