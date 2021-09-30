c86c5a8751c66a93dd4a582f0cefb7255a688a05
AMYrgo <89993459+AMYrgo@users.noreply.github.com>
2021-09-30 15:46:30 +0200

    Update tempclient.py

--- temperatureguage/tempclient.py
+++ temperatureguage/tempclient.py
@@ -1,5 +1,3 @@
-"""Author: Alexander Mörch"""
-
 #Modul som läser och omvandlar värdet från en temperaturgivare
 import tempguage as temp
 #Modul som hanterar MQTT-implementation
@@ -11,17 +9,16 @@ client = mqtt.Client()
 
 #Ansluter till en broker --> HiveMQ idetta fallet
 #client.connect("broker.hivemq.com", port=1883, keepalive=60)
-client.connect("192.168.56.1",port=1883, keepalive=60)
+client.connect("192.168.0.155",port=1883, keepalive=60)
 
 #Startar bakgrundsprocess som hanterar kommunikation med brokern
 client.loop_start()
 
 while True:
-    #Läser data från temperaturgivaren --> Se tempguage-modulen
-    var = temp.read_temp()
 
-    #Anger topic, vad som skall skickas, och quality of service
-    msg1 = client.publish("yrgo/ela20/Dark/hello", payload=var, qos=1)
+    var = temp.sensor_package()
+
+    msg1 = client.publish("sensors/temperature/channel1", payload=var, qos=1)
 
     #Väntar en sekund
     time.sleep(1)
