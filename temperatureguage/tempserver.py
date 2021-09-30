655327bdf31a74a154477beaf30847715592e0cc
AMYrgo <89993459+AMYrgo@users.noreply.github.com>
2021-09-30 15:47:12 +0200

    Update tempserver.py

--- temperatureguage/tempserver.py
+++ temperatureguage/tempserver.py
@@ -1,19 +1,23 @@
-"""Alexander Mörch"""
-
+import struct
 import paho.mqtt.client as mqtt
 
-#Sätter topic och quality of service
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("yrgo/ela20/Dark/hello", qos=1)
+    client.subscribe("sensors/temperature/channel1", qos=1)
 
-#Skriver ut meddelande på terminalen
 def on_message(client, userdata, msg):
-    print(f"{float(msg.payload)}")
+    #print(f"{float(msg.payload)}")
+    var = msg.payload
+    var1 = var.decode("UTF-8") 
+    with open("temperature.txt", "a") as file:
+        file.write(f"{var1}\n")   
+        #file.write(f"{float(msg.payload)}\n")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-#client.connect("192.168.0.155", port=1883, keepalive=60)
-client.connect("broker.hivemq.com", port=1883, keepalive=60)
+#client.connect("192.168.0.155", port=1883, keepalive=60) 
+#client.connect("broker.hivemq.com", port=1883, keepalive=60)
+client.connect("localhost", port=1883, keepalive=60)
 client.loop_forever()
+
