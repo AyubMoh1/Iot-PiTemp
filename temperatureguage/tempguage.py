296731adb328a17e1b60a1c6a8b54fb054511db4
AyubMoh1 <89800658+AyubMoh1@users.noreply.github.com>
2021-10-01 10:55:39 +0200

    Update tempguage

--- temperatureguage/tempguage.py
+++ temperatureguage/tempguage.py
@@ -13,7 +13,8 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-device_ID = device_folder.replace(base_dir,'')
+device_ID = device_folder.replace(base_dir,'') #Hittar ID
+S_ID = device_ID.replace('-','') #Tar bort '-' från ID
 
 #def get_sensor_ID():
 #    with open('/sys/bus/w1/devices/28-0000095c43dd/id',"r") as file:
@@ -33,19 +34,34 @@ def read_temperature():
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
+        temp_c = temp_string #float(temp_string) / 1000.0
         return temp_c
 
 def sensor_package():
     temperature = read_temperature()
     tid = get_datetime_string()
-    A = format_frame(device_ID, tid ,temperature)
-    return bytearray(A, "UTF-8")
+    return struct.pack('QIBI', S_ID, tid, index, temperature)
+    #A = format_frame(device_ID, tid ,temperature)
+    #return bytearray(A, "UTF-8")
 
 #Tar in
-def format_frame(A,B,C):
-    return 'SensorID: ' + A + ' Time: '+ B + ' Temperature: ' + str(C) + ' C'
+#def format_frame(A,B,C):
+   # return struct.pack('iii', A,B,C)
+    #return 'SensorID: ' + A + ' Time: '+ B + ' Temperature: ' + str(C) + ' C'
 
 def get_datetime_string():
-    dt = datetime.datetime.now()
-    return dt.strftime("%d/%m/%Y %H:%M:%S")
+    #dt = datetime.datetime.now()
+    #return dt.strftime("%d/%m/%Y %H:%M:%S")
+    dt = int(time.time())
+    return dt
+
+
+#var = '0x'+ device_ID
+#var = (var, 0)
+#num = (device_ID, 16)
+var = int(S_ID, 16)
+print(var)
+print(get_datetime_string())
+print(read_temperature())
+#print(device_ID)
+#print(num)
