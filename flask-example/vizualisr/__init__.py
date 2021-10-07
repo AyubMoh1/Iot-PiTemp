e9f558cb9e0c40e7be306851e370d4f30fb6e6e3
AMYrgo <89993459+AMYrgo@users.noreply.github.com>
2021-10-07 19:14:30 +0200

    Update __init__.py

--- flask-example/vizualisr/__init__.py
+++ flask-example/vizualisr/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, flash
 import datetime
-import sys
-import os
-import csv
-#importera filen server.py ex. import Iot-PiTemp.temperatureguage.tempserver as server
+import time
 
-txtPath = "c:/Users/chartedge/Desktop/Iot-PiTemp-main/flask-example/vizualisr/temperature.txt"
+#Anger sökvägen där textfilen med data ligger 
+txtPath = "D:/Documents/Tempprojekt/flask-example/vizualisr/temperature.txt"
+
+#Sätt miljövariabel till: $Env:FLASK_APP="vizualisr"
+
 # This creates the flask application and configures it
 # flask run will use this to start the application properly
 app = Flask(__name__)
@@ -13,80 +14,66 @@ app.config.from_mapping(
     # This is the session key. It should be a REALLY secret key!
     SECRET_KEY="553e6c83f0958878cbee4508f3b28683165bf75a3afe249e"
 )
-# The mapping of units in accordance with our specification
-UNITS = {
-    0: "°C",
-    1: "RH"
-}
 
-
-def read_and_split_string():
+#Funktionen öppnar textfilen med data och skikcar varje rad till en 
+#funktion som delar upp varje textrad i fem delar
+#Basserat på ett index n skickar den tillbaka sensorID och kanalindex, 
+#eller tidsstämpel, temperatur och enhet
+def get_data(data, n):
+    test = 0
     with open(txtPath,"r") as file:
-        string_1 = file.readline()
-        list_1 = string_1.split()
-        return list_1
+        temperature_data = file.readlines()
+        for line in temperature_data:
+            id, i, t, d, u = split_data(line)
+            id = str(id)
+            d = float(d)
+            d = d/1000
+            t = int(t, 16)
+            t = datetime.datetime.fromtimestamp(t)
+            if n == "id_and_index":
+                if (id,i) not in data:    
+                    data.append((id, i))
+            if n == "time_temperature_unit":
+                data.append((t, d, u))
+    return data
 
-print(read_and_split_string())
-
-# This is a placeholder that returns a fixed set of meters
-# in a proper system this would look in a database or in
-# the file system for a list of meters in the system
+#Funktionen kallar till funtionen get_data som, basserat på en textsträng
+#appendar en lista med sensorID och kanalindex. Listan returneas sedan. 
 def get_meters():
-    
-   
-    meters = [ ("2123", 0),
-               ("1234", 1),
-               ("1235", 0),
-               ("1236", 0)]
-    return meters
+    list_of_sensors = []
+    list_of_sensors = get_data(list_of_sensors,"id_and_index")
+    return list_of_sensors
 
-# This is a placeholder that returns a fixed set of 
-# measurement data. In a proper system this would read
-# the data from a database or the file system
+#Funktionen tar in en textsträng och lägger in alla orden i textsträngen 
+# i en separat lista. Listan delas sedan upp och returneras som en tuple.
+#En förbättrad variant av denna hade kanske kunnat lösas med en for-loop.
+def split_data(line):
+    line_list = line.split()
+    sensor_id = line_list[0]
+    time = line_list[1]
+    index = line_list[2]
+    data = line_list[3]
+    unit = line_list[4]
+    return (sensor_id, index, time, data, unit)
+
+#Denna funktionen anropar funktionen get data med en variabel som gör att funktionen returnerar 
+# tidsslag, temperatur och enhet. Datan läggs in i en lista som till sist vänds uppochner
+#Listan vänds uppochner för att den nya datan skall visas högst upp i visningsprogrammet.
+#Funktionen returnerar den korrigerade listan. 
 def get_measurements(meter, channel):
-    if (meter, int(channel)) not in get_meters():
-        # the function flash() is part of the flask system and lets us
-        # register error/warning messages that should be shown on the
-        # web page.
-        flash(f"The meter {meter} with channel {channel} does not exist.")
-        return []
-
-    # this just generates a fixed set of measurement values
-    # to have something to show...
     measurements = []
-    time = 1624537020
-    for _ in range(20):
-        date = datetime.datetime.fromtimestamp(time)
-        value = time % 27
-        measurements.append((date, value, UNITS[0]))
-        time = time - 10 * 60
+    measurements = get_data(measurements,"time_temperature_unit")
+    measurements.reverse()
     return measurements
 
-# @app.route registers a handler for a specific URL
-# in this case the URL / (i.e. the root of the server)
-
 @app.route("/")
+#Funktionen sätter upp en visningssida som radar upp samtliga sensorer som detekteras i en textfil. 
 def start_page():
     meters = get_meters()
     return render_template("start.html", meters=meters)
 
-# using @app.route with <something> makes "something" into
-# a path variable. In the case /meter/1234/channel/5678
-# the meter-argument would be set to (the string!) 1234
-# and channel to 5678.
-
-
-
 @app.route("/meter/<meter>/channel/<channel>")
-def show_measurements(meter, channel)
-    measurements = []
-    list_3 = read_and_split_string()
-    var3 = float(list_3[3])
-    var3 = var3 / 1000
-    var_2 = int(list_3[1], 16)
-    time = datetime.datetime.fromtimestamp(var_2)
-    measurements.append((time, var3, UNITS[0]))
-    #measurements.append((list_3[1], list_3[3], UNITS[0]))
-    meter = list_3[0]
-    channel = list_3[2]
-    return render_template("meter.html", meter=meter, channel=channel, measurements=measurements)
+#Funktionen presenterar datan i webbserverns visningssida
+def show_measurements(meter, channel):
+    measurements = get_measurements(meter, channel)
+    return render_template("meter.html", meter=meter, channel=channel, measurements=measurements)    
