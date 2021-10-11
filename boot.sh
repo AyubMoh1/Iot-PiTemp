1d18fb7ddeda665a2fda77e4c5c65c2f576a03a1
AyubMoh1 <89800658+AyubMoh1@users.noreply.github.com>
2021-10-11 17:55:04 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -2,5 +2,22 @@
 
 ssh pi@192.168.1.11 /home/pi/temperatureguage/bootclient.sh &
 
+cd server
+
+pipenv install
+
+pipenv run python tempserver.py &
+
+cd -
+
+cd flask-example
+
+pipenv install
+
+
+export FLASK_APP=vizualisr
+
+pipenv run flask run
+
 
 echo "done!"
