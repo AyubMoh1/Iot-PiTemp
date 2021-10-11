6a705a05713b86142ec4a7c963638669944eb107
AMYrgo <89993459+AMYrgo@users.noreply.github.com>
2021-10-11 16:14:44 +0200

    Update tempserver.py

--- server/tempserver.py
+++ server/tempserver.py
@@ -1,27 +1,18 @@
 import struct
 import paho.mqtt.client as mqtt
+import datetime
 
-txtPath = "c:/Users/aom/Desktop/Iot-PiTemp-main/flask-example/vizualisr/temperature.txt"
+txtPath = "D:/Documents/Tempprojekt/flask-example/vizualisr/temperature.txt"
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe("sensors/temperature/channel1", qos=1)
+    #client.subscribe("measurement/280000095c43dd", qos=1)
 
 
 def on_message(client, userdata, msg):
-    #print(f"{float(msg.payload)}")
-    #var = msg.payload
-    #var1 = var.decode("UTF-8") 
-    a,b,c,d = struct.unpack("QIBI", msg.payload)
-    #a,b,c,d,e = struct.unpack('QIBII', S_ID, tid, index, temperature, unit)
-    #a1=f"{a:x}"
-    
+    a, b, c, d, e = struct.unpack("QIBIB", msg.payload)
     with open(txtPath, "a") as file:
-        #file.write(f"{var1}\n")   
-        #file.write(f"{a:x} {b:x} {c:x} {d:x} {e:x}\n")
-        file.write(f"{a:x} {b:x} {c:x} {d:x}\n")
-
-
-
+        file.write(f"{a:x} {b:X} {c:x} {d:x} {e:x}\n")
 
 
 client = mqtt.Client()
@@ -32,4 +23,3 @@ client.on_message = on_message
 #client.connect("broker.hivemq.com", port=1883, keepalive=60)
 client.connect("localhost", port=1883, keepalive=60)
 client.loop_forever()
-
